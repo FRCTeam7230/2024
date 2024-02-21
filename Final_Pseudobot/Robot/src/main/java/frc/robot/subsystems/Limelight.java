3175c28bc2633c0ca5bfbc1fd581f15034a37e50
MultiOSDev <81823905+MultiOSDev@users.noreply.github.com>
2024-02-20 18:07:09 -0800

    it works! (for the most part)

--- Final_Pseudobot/Robot/src/main/java/frc/robot/subsystems/Limelight.java
+++ Final_Pseudobot/Robot/src/main/java/frc/robot/subsystems/Limelight.java
@@ -5,21 +5,22 @@ import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 /* TODO:
  * - set up the general limelight [x (maybe? i dont know ill test with the limelight)]
- * - set up the apriltag detection []
- * - set up the red blue team integration [x (its just a skeleton right now ill implement apriltags into this with the limelight)]
+ * - set up the apriltag detection [--68%--]
+ * - set up the red blue team integration [x]
  * - test this integration with the apriltags and the red blue team chooserand integration []
  */
 public class Limelight {
     private static NetworkTableInstance table = null;
-    public static double targetX, targetY, visionTargets;
+    public static double targetX, targetY, visionTargets, targetID;
     public static double targetArea;
+    
     public static void limelight(String teamColor) {
         // this is where we set which pipeline to use based on the team color that gets passed here, the
         // rest of the code works off of this
         if (teamColor == "red") {
-            // get red team pipeline
+            NetworkTableInstance.getDefault().getTable("limelight").getEntry("pipeline").setNumber(0); // pipeline 0 is for red team
         } else if (teamColor == "blue") {
-            // get blue team pipeline
+            NetworkTableInstance.getDefault().getTable("limelight").getEntry("pipeline").setNumber(1); // pipeline 1 is for blue team
         }
     }
     public static void refreshData() {
@@ -27,18 +28,16 @@ public class Limelight {
         NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
         NetworkTableEntry tx = table.getEntry("tx");
         NetworkTableEntry ty = table.getEntry("ty");
-        // NetworkTableEntry ta = table.getEntry("ta"); (commented out for now, we might not need this)
         NetworkTableEntry tv = table.getEntry("tv");
+        NetworkTableEntry tid = table.getEntry("tid");
         
         // read values periodically
         visionTargets = tv.getDouble(0.0);
         targetX = tx.getDouble(0.0);
         targetY = ty.getDouble(0.0);
-        // targetArea = ta.getDouble(0.0); (commented out for now, we might not need this)
+        targetID = tid.getDouble(0.0);
+        // add tid here
     }
-    // public static double[] updateTarget(){
-        // this refreshes what apriltags it should see (i hope)
-    // }
 
     public static void manualLimelightOff(boolean manualLayout){
         if (manualLayout){
@@ -60,6 +59,21 @@ public class Limelight {
         refreshData();
         return targetY;
     } // this gets the apriltag's y angle offset
+
+    public static double getTargetID(){
+        refreshData();
+        return targetID;
+    } // this gets the apriltag's ID
+
+    public static double apriltagDistance() {
+        // this is where we calculate the distance to the apriltag
+        double h1 = 0; // height of the limelight from the ground
+        double h2 = 0; // height of the apriltag from the ground
+        double a1 = 0; // angle of the limelight
+        double a2 = 0; // angle of the apriltag
+        double d = (h2 - h1) / Math.tan(a1 + a2);
+        return d;
+    } // this is where we calculate the distance to the apriltag
 
     //public static boolean proceedMoving(double objAreaRequired) {
         // refreshData();
